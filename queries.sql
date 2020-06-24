a8a1d9e904372817247b635be6ec9ad762d499d5
Rob <rlau929@gmail.com>
2020-06-24 13:29:56 -0700

    Adding queries.sql file

---
+++ queries.sql
@@ -0,0 +1,89 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR (50) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY  (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+DROP TABLE dept_emp CASCADE;
+COPY employees FROM 'C:\Program Files\Data\employees.csv' DELIMITER ',' CSV HEADER;
+COPY dept_emp FROM 'C:\Program Files\Data\dept_emp.csv' DELIMITER ',' CSV HEADER;
+COPY dept_manager FROM 'C:\Program Files\Data\dept_manager.csv' DELIMITER ',' CSV HEADER;
+COPY salaries FROM 'C:\Program Files\Data\salaries.csv' DELIMITER ',' CSV HEADER;
+COPY titles FROM 'C:\Program Files\Data\titles.csv' DELIMITER ',' CSV HEADER;
+
+SELECT * FROM departments;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
